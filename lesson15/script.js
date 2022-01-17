15a4ca6189f2a2a939ee62687e34793b903d1cc7
svyat1234 <75384046+svyat1234@users.noreply.github.com>
2022-01-17 11:53:45 +0300

    Add files via upload

---
+++ lesson15/script.js
@@ -0,0 +1,15 @@
+const DomElement = function () {
+    this.selector 
+    this.height 
+    this.width 
+    this.bg
+    this.fontSize 
+
+}
+
+DomElement.prototype.check = function () {
+
+}
+
+const newCheck = new DomElement
+console.log(newCheck);
