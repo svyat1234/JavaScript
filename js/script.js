49937dba1ec8f569dd024a7a18abb361fd9450c1
svyat1234 <75384046+svyat1234@users.noreply.github.com>
2022-02-08 15:26:27 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+title = document.getElementsByTagName('h1')
+console.log(title);
+button = document.getElementsByClassName('handler_btn')
+console.log(button);
+buttonTiming = document.querySelector('.screen-btn')
+console.log(buttonTiming);
+percent = document.querySelectorAll('.other-items.percent')
+number = document.querySelectorAll('.other-items.number')
+console.log(percent,number);
+range = document.querySelector('.rollback input')
+console.log(range);
+span = document.querySelector('.rollback .range-value')
+console.log(span);
+input = document.getElementsByClassName('total-input')
+console.log(input);
+let = document.querySelectorAll('.screen')
+console.log(let);
