c0776782c40bb0766768a5fde92461b9824f656f
svyat1234 <75384046+svyat1234@users.noreply.github.com>
2022-02-09 10:53:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let title = prompt('Как называется ваш проект?');
-let screens  = prompt('Какие типы экранов нужно разработать ?' , 'Простые, Сложные, Интерактивные')
-let screenPrice  = +prompt('Сколько будет стоить данная работа?')
-let servicePrice1 = +prompt('Сколько это будет стоить?')
-let servicePrice2 = +prompt('Сколько это будет стоить?')
+let title = prompt("Как называется ваш проект?")
+let screens = prompt("Какие типы экранов нужно разработать?", "Простые, сложные, интерактивные")
+let screenPrice = prompt("Сколько будет стоить данная работа?")
+let adaptive = prompt("Нужен ли адаптив на сайте?")
+let service1 = prompt("Какой дополнительный тип услуги нужен?")
+let servicePrice1 = prompt("Сколько это будет стоить?")
+let service2 = prompt("Какой дополнительный тип услуги нужен?")
+let servicePrice2 = +prompt("Сколько это будет стоить?")
 let rollback = 10
 let fullPrice = screenPrice + servicePrice1 + servicePrice2
 
